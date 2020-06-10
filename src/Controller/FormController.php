2e5659f002c13460f37790e94c79363637e4e0cd
gg-coder <gyangoerdin@live.nl>
2020-06-10 12:09:06 +0200

    changes

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -7,23 +7,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Post;
 use App\Form\PosttType;
+use Symfony\Component\HttpFoundation\Request;
 
 class FormController extends AbstractController
 {
     /**
      * @Route("/form", name="form")
      */
-    public function index()
+    public function index(Request $request)
     {
         $post = new Post();
         $post->setName('Welcome');
         $post->setAge('18');
 
-        $form = $this->createForm(PosttType::class, $post);
+        $form = $this->createForm(PosttType::class, $post[
+        'action' => $this->generateUrl('form')
+
+            ]);
 
         //handle the request//
+        $form->handleRequest($request);
 
 
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            // saving to the database //
+            $en = $this->getDoctrine()->getManager();
+
+            $en->persist($post);
+            $en->flush();
+        }
         return $this->render('form/index.html.twig', [
             'post_form' => $form->createView()
         ]);
